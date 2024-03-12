4dbb65662fbe270092151d8c0a4a943748880e49
ArloZ <arlozql@gmail.com>
2024-03-12 09:56:07 +0800

    add middleware

--- trans-service/cmd/app/main.go
+++ trans-service/cmd/app/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/arloz/trans-service/src/handler"
+	"github.com/arloz/trans-service/src/middleware"
 
 	"github.com/labstack/echo/v4"
 )
@@ -13,6 +14,9 @@ func main() {
 	// 初始化所有的请求处理映射
 	initHandlers(e)
 
+	// 初始化中间件
+	initMiddleware(e)
+
 	// 在端口8090启动服务
 	e.Logger.Fatal(e.Start(":8090"))
 }
@@ -20,4 +24,9 @@ func main() {
 func initHandlers(e *echo.Echo) {
 	// 翻译文本 /api/translate?content=xxx
 	e.GET("/api/translate", handler.Translate)
+}
+
+func initMiddleware(e *echo.Echo) {
+	// 鉴权校验
+	e.Use(middleware.AuthChecker())
 }
